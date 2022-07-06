56b6e08ee93824535a8bf2c8feaaf0689bb3ef93
DAVID PC-CAJA <davidortiz25122010@gmail.com>
2022-07-06 07:30:25 -0400

    Loguearse y Cerrar Session

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -497,9 +497,9 @@
                                 </li> --}}
 
                                 <!-- User -->
-                                <li class="nav-item lh-1 me-3">
+                                {{-- <li class="nav-item lh-1 me-3">
                                     <a href="{{('login')}}">Iniciar Sesion</a>
-                                </li>
+                                </li> --}}
                                 <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                     <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                                         <div class="avatar avatar-online">
@@ -550,10 +550,18 @@
                                             <div class="dropdown-divider"></div>
                                         </li>
                                         <li>
-                                            <a class="dropdown-item" href="auth-login-basic.html">
+                                            <form action="{{ route('logout') }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="dropdown-item" href="auth-login-basic.html">
+                                                    <i class="bx bx-power-off me-2"></i>
+                                                    Cerrar Session
+                                                    {{-- <span class="align-middle">Log Out</span> --}}
+                                                </button>
+                                            </form>
+                                            {{-- <a class="dropdown-item" href="auth-login-basic.html">
                                                 <i class="bx bx-power-off me-2"></i>
                                                 <span class="align-middle">Log Out</span>
-                                            </a>
+                                            </a> --}}
                                         </li>
                                     </ul>
                                 </li>
@@ -623,22 +631,24 @@
 
         <!-- Core JS -->
         <!-- build:js assets/vendor/js/core.js -->
-        <script src="../assets/vendor/libs/jquery/jquery.js"></script>
-        <script src="../assets/vendor/libs/popper/popper.js"></script>
-        <script src="../assets/vendor/js/bootstrap.js"></script>
-        <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+        @livewireScripts
 
-        <script src="../assets/vendor/js/menu.js"></script>
+        <script src="{{asset('assets/vendor/libs/jquery/jquery.js')}}"></script>
+        <script src="{{asset('assets/vendor/libs/popper/popper.js')}}"></script>
+        <script src="{{asset('assets/vendor/js/bootstrap.js')}}"></script>
+        <script src="{{asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+
+        <script src="{{asset('assets/vendor/js/menu.js')}}"></script>
         <!-- endbuild -->
 
         <!-- Vendors JS -->
-        <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+        <script src="{{asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
 
         <!-- Main JS -->
-        <script src="../assets/js/main.js"></script>
+        <script src="{{asset('assets/js/main.js')}}"></script>
 
         <!-- Page JS -->
-        <script src="../assets/js/dashboards-analytics.js"></script>
+        <script src="{{asset('assets/js/dashboards-analytics.js')}}"></script>
 
         <!-- Place this tag in your head or just before your close body tag. -->
         <script async defer src="https://buttons.github.io/buttons.js"></script>
